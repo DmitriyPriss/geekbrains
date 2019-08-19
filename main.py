42841dfd9fcf24430d0c2fd599b6b68a3ad26dcb
Dmitriy <nakky59rus@mail.ru>
2019-08-19 13:07:41 +0500

    2 task

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import sys
+
+list = [4, 1, 11, 5, 8, 15, 7, 13, 12, 16, 14, 2, 9, 6, 3, 10]
+secret_list = ['|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|',
+               '*', '|', '*', '|', '*', '|', '*', '|', '*', '|', '*', '|']
+answer2 = ''
+answer3 = ''
+char = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
 print('Своя игра')
+
 gamer = {'name': input('Как вас зовут?\n'),
          'age': int(input('Сколько тебе лет?\n')),
          'sex': input('Какого ты пола?\n'),
@@ -6,33 +16,58 @@ gamer = {'name': input('Как вас зовут?\n'),
          'love_games': input('Любишь ли ты играть?\n'),
          }
 
-question2 = ''
-
 if gamer['age'] < 18:
     print('Тебе нет 18 лет, тебе нельзя играть')
 
 elif gamer['age'] > 90:
-    question1 = input('Игра может быть сильно утомительной для вас.\nУверены ли, вы, что хотите играть?\n')
+    answer1 = input('Игра может быть сильно утомительной для вас.\nУверены ли, вы, что хотите играть?\n')
+    if answer3 == 'exit':
+        sys.exit()
 
-    if question1 == 'Да':
-        question2 = input('Точно?\n')
+    if answer1.lower() == 'Да'.lower():
+        answer2 = input('Точно?\n')
 
-        if question2 == 'Да':
+        if answer2.lower() == 'Да'.lower():
             print('Хорошо, тогда начнём игру!')
+
+        if answer2 == 'exit':
+            sys.exit()
 
         else:
             print('До свидания,', gamer['name'])
 
+    if answer1 == 'exit':
+        sys.exit()
     else:
         print('До свидания,', gamer['name'])
 
-if 18 <= gamer['age'] <= 90 or question2 == 'Да':
+if 18 <= gamer['age'] <= 90 or answer2.lower() == 'Да'.lower():
     print('Привет,', gamer['name'], '\nЯ могу назвать буквы алфавита, которых нет в твоем имени.')
-    char = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     for i in gamer['name']:
         if i in char:
             char = char.replace(i, '')
 
 print('В твоём имени нет букв:')
-for i in char:
-    print(i)
+print(char)
+print('Я задумал 16 чисел от 1 до 16 и расположил их в произвольном порядке в строке. Скажи мне где какое.')
+print('|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|')
+for i in range(len(list)):
+    attempt = 0
+    index = list.index(i + 1) + 1
+    answer3 = input('Скажи мне, где число {}?\n'.format(i + 1))
+    if answer3 == 'exit':
+        sys.exit()
+
+    answer3 = int(answer3)
+    while answer3 != index:
+        attempt = attempt + 1
+        answer3 = input('Не угадал! Попробуй ещё раз!\n')
+        if answer3 == 'exit':
+            sys.exit()
+
+        answer3 = int(answer3)
+    print('Правильно! Ты угадал с', attempt+1, 'раза!!!')
+    secret_list[index * 2 - 1] = str(i + 1)
+    print(''.join(secret_list))
+
+print('Ты победил!!!')
